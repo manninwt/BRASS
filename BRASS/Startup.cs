10fbd3edfb85e0e872d4b59a38267fb8aa0653c6
Nathan Boehringer <boehrinw@mail.uc.edu>
2020-04-19 03:56:36 -0400

    Added api call to convert address to geolocation Added token info to the appsettings

--- BRASS/Startup.cs
+++ BRASS/Startup.cs
@@ -37,6 +37,8 @@ namespace BRASS
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IConfiguration>(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
